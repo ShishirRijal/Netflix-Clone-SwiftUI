fe3494ba3119abafa1aecf934d2b54154531c224
Shishir Rijal <ccrrizal0892@gmail.com>
2024-10-22 01:23:53 +0545

    Initial Commit

---
+++ Netflix/NetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixApp.swift
+//  Netflix
+//
+//  Created by Shishir Rijal on 13/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct NetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
