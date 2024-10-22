96014ff034f46700711d1cae038272e3f6a87951
Shishir Rijal <ccrrizal0892@gmail.com>
2024-10-22 14:08:59 +0545

    refactor home view

--- Netflix/Views/Home/HomeView.swift
+++ Netflix/Views/Home/HomeView.swift
@@ -1,101 +1,53 @@
-//
-//  HomeView.swift
-//  Netflix
-//
-//  Created by Shishir Rijal on 22/10/2024.
-//
-
 import SwiftUI
 import SDWebImageSwiftUI
 
-
 struct HomeView: View {
-
-  let posterImageLinks = [
-      "https://image.tmdb.org/t/p/w500/wTnV3PCVW5O92JMrFvvrRcV39RU.jpg",
-      "https://image.tmdb.org/t/p/w500/b33nnKl1GSFbao4l3fZDDqsMx0F.jpg",
-      "https://image.tmdb.org/t/p/w500/lqoMzCcZYEFK729d6qzt349fB4o.jpg",
-      "https://image.tmdb.org/t/p/w500/8cdWjvZQUExUUTzyp4t6EDMubfO.jpg",
-      "https://image.tmdb.org/t/p/w500/qpdFKDvJS7oLKTcBLXOaMwUESbs.jpg",
-      "https://image.tmdb.org/t/p/w500/kKgQzkUCnQmeTPkyIwHly2t6ZFI.jpg",
-      "https://image.tmdb.org/t/p/w500/izuzUb0sDokqp9o8utVfsrSJuy5.jpg",
-      "https://image.tmdb.org/t/p/w500/wWba3TaojhK7NdycRhoQpsG0FaH.jpg",
-      "https://image.tmdb.org/t/p/w500/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-      "https://image.tmdb.org/t/p/w500/l1175hgL5DoXnqeZQCcU3eZIdhX.jpg",
-      "https://image.tmdb.org/t/p/w500/iz2GabtToVB05gLTVSH7ZvFtsMM.jpg",
-      "https://image.tmdb.org/t/p/w500/3EpZ2ksjijmdr8BhISP03PYzNFW.jpg",
-      "https://image.tmdb.org/t/p/w500/hPIWQT70wQK6akqfLXByEvr62u0.jpg"
-  ]
+    let posterImageLinks = [
+        "https://image.tmdb.org/t/p/w500/wTnV3PCVW5O92JMrFvvrRcV39RU.jpg",
+        "https://image.tmdb.org/t/p/w500/b33nnKl1GSFbao4l3fZDDqsMx0F.jpg",
+        "https://image.tmdb.org/t/p/w500/lqoMzCcZYEFK729d6qzt349fB4o.jpg",
+        "https://image.tmdb.org/t/p/w500/8cdWjvZQUExUUTzyp4t6EDMubfO.jpg",
+        "https://image.tmdb.org/t/p/w500/qpdFKDvJS7oLKTcBLXOaMwUESbs.jpg",
+        "https://image.tmdb.org/t/p/w500/kKgQzkUCnQmeTPkyIwHly2t6ZFI.jpg",
+        "https://image.tmdb.org/t/p/w500/izuzUb0sDokqp9o8utVfsrSJuy5.jpg",
+        "https://image.tmdb.org/t/p/w500/wWba3TaojhK7NdycRhoQpsG0FaH.jpg",
+        "https://image.tmdb.org/t/p/w500/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
+        "https://image.tmdb.org/t/p/w500/l1175hgL5DoXnqeZQCcU3eZIdhX.jpg",
+        "https://image.tmdb.org/t/p/w500/iz2GabtToVB05gLTVSH7ZvFtsMM.jpg",
+        "https://image.tmdb.org/t/p/w500/3EpZ2ksjijmdr8BhISP03PYzNFW.jpg",
+        "https://image.tmdb.org/t/p/w500/hPIWQT70wQK6akqfLXByEvr62u0.jpg"
+    ]
 
     var body: some View {
-      NavigationStack {
-        ScrollView {
-
-          ZStack(alignment: .top) {
-
-            // Hero Image
-            GeometryReader { geometry in
-              Image("CaptainMarvel")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: geometry.size.width, height: geometry.size.height*1.2 )
-                .overlay(
-                  LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.3), Color.black.opacity(0.2), Color.black.opacity(0.8)]), startPoint: .top, endPoint: .bottom)
-                )
-
+        NavigationStack {
+            ScrollView {
+                HeroSection()
+                Spacer().frame(height: 50)
+              MoviesAndShowsView(posterImageLinks: posterImageLinks)
             }
-            
-            VStack {
-              Spacer()
-              
-              // Movie Name and Tags
-              Text("Captain Marvel")
-                .font(.heroHeaderFont)
-                .lineLimit(2)
-                .padding(.bottom, 3)
-              Text("Sci-Fi • Adventure • Fantasy")
-                .font(.customFont(.light, 16))
-                .padding(.bottom, 30)
+            .preferredColorScheme(.dark)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Image("LetterLogo")
+                        .resizable()
+                        .frame(width: 20, height: 32)
+                }
 
+                ToolbarItem(placement: .topBarTrailing) {
+                    HStack(spacing: 20) {
+                        Image(systemName: "airplayvideo")
+                            .resizable()
+                            .frame(width: 24, height: 24)
+                            .foregroundColor(.white)
 
-              HomeViewButtons()
+                        Image("ProfileIcon")
+                            .resizable()
+                            .frame(width: 24, height: 24)
+                    }
+                }
             }
-            .padding(.top, 30)
-            .frame(width: .infinity, height: UIScreen.main.bounds.height*0.6)
-
-
-          }
-          Spacer()
-            .frame(height: 50)
-
-          ContinueWatching(posterImageLinks: posterImageLinks)
-
-
+            .ignoresSafeArea()
         }
-        .preferredColorScheme(.dark)
-        .toolbar {
-          ToolbarItem(placement: .topBarLeading) {
-            Image("LetterLogo")
-              .resizable()
-              .frame(width: 20, height: 32)
-          }
-
-          ToolbarItem(placement: .topBarTrailing) {
-            HStack(spacing: 20) {
-              Image(systemName: "airplayvideo")
-                .resizable()
-                .frame(width: 24, height: 24)
-                .foregroundColor(.white)
-
-              Image("ProfileIcon")
-                .resizable()
-                .frame(width: 24, height: 24)
-            }
-          }
-        }
-        
-      .ignoresSafeArea()
-      }
     }
 }
 
@@ -103,10 +55,103 @@ struct HomeView: View {
     HomeView()
 }
 
+// MARK: - Hero Section
+struct HeroSection: View {
+    var body: some View {
+        ZStack(alignment: .top) {
+            GeometryReader { geometry in
+                Image("CaptainMarvel")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: geometry.size.width, height: geometry.size.height * 1.2)
+                    .overlay(
+                        LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.3), Color.black.opacity(0.2), Color.black.opacity(0.8)]), startPoint: .top, endPoint: .bottom)
+                    )
+            }
 
+            VStack {
+                Spacer()
+
+                // Movie Name and Tags
+                Text("Captain Marvel")
+                    .font(.heroHeaderFont)
+                    .lineLimit(2)
+                    .padding(.bottom, 3)
+                Text("Sci-Fi • Adventure • Fantasy")
+                    .font(.customFont(.light, 16))
+                    .padding(.bottom, 30)
+
+                HomeViewButtons()
+            }
+            .padding(.top, 30)
+            .frame(width: .infinity, height: UIScreen.main.bounds.height * 0.6)
+        }
+    }
+}
+
+// MARK: - Movies And Shows View
+struct MoviesAndShowsView: View {
+    let posterImageLinks: [String]
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            CategoryView(title: "Continue Watching", posterImageLinks: posterImageLinks, isWatching: true)
+            CategoryView(title: "Netflix Originals", posterImageLinks: posterImageLinks)
+            CategoryView(title: "Popular on Netflix", posterImageLinks: posterImageLinks)
+            CategoryView(title: "Trending Now", posterImageLinks: posterImageLinks)
+            CategoryView(title: "TV Shows", posterImageLinks: posterImageLinks)
+        }
+    }
+}
+
+// MARK: - Category View
+struct CategoryView: View {
+    let title: String
+    let posterImageLinks: [String]
+    var isWatching: Bool = false
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(.titleFont)
+                .foregroundColor(.white)
+                .padding(.bottom, 10)
+
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 10) {
+                    ForEach(posterImageLinks.shuffled(), id: \.self) { link in
+                        ZStack(alignment: .topLeading) {
+                            ZStack(alignment: .center) {
+                                WebImage(url: URL(string: link))
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 106, height: 152)
+                                    .clipped()
+                                    .overlay(.grayDark1.opacity(0.1))
+                                if isWatching {
+                                    Image(systemName: "play.circle")
+                                        .resizable()
+                                        .frame(width: 50, height: 50)
+                                }
+                            }
+                            .cornerRadius(8)
+
+                            // Top left logo
+                            Image("LetterLogo")
+                                .resizable()
+                                .frame(width: 18, height: 30)
+                                .padding(5)
+                        }
+                    }
+                }
+            }
+        }
+        .padding(.bottom, 20)
+    }
+}
+
+// MARK: - Home View Buttons
 struct HomeViewButtons: View {
-
-    // Reusable button view for My List and Info buttons
     private func iconButton(image: String, text: String, action: @escaping () -> Void) -> some View {
         Button(action: action) {
             VStack {
@@ -128,10 +173,9 @@ struct HomeViewButtons: View {
 
             Spacer()
 
-            // Play Button
-          CustomButton(title: "Play", image: "play.fill") {
-                          print("Play button tapped")
-                      }
+            CustomButton(title: "Play", image: "play.fill") {
+                print("Play button tapped")
+            }
 
             Spacer()
 
@@ -142,34 +186,3 @@ struct HomeViewButtons: View {
         .frame(width: UIScreen.main.bounds.width * 0.7)
     }
 }
-
-
-
-struct ContinueWatching: View {
-  let posterImageLinks: [String]
-
-  var body: some View {
-    VStack (alignment: .leading) {
-      Text("Continue Watching")
-        .font(.titleFont)
-        .foregroundColor(.white)
-      
-      ScrollView(.horizontal) {
-        HStack(spacing: 10) {
-
-          ForEach(1..<posterImageLinks.count){index in
-            WebImage(url: URL(string: posterImageLinks[index]))
-              .resizable()
-              .aspectRatio(contentMode: .fill)
-              .frame(width: 106, height: 188)
-              .clipped()
-          }
-
-        }
-      }
-    }
-  }
-}
-
-
-
