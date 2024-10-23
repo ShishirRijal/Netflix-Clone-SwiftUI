4588b32ffba58c3fe78d221adec472e1f2194384
Shishir Rijal <ccrrizal0892@gmail.com>
2024-10-24 01:34:55 +0545

    add new and hot viewmodel

---
+++ Netflix/ViewModels/NewAndHotViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  NewAndHotViewModel.swift
+//  Netflix
+//
+//  Created by Shishir Rijal on 24/10/2024.
+//
+
+import Foundation
+
+class NewAndHotViewModel: ObservableObject {
+    @Published var movies: [Movie] = []
+    @Published var activeTag: String = "upcoming" // Track the active tag
+    @Published var loading: Bool = false
+    init() {
+        fetchMovies()
+    }
+
+  // Function to fetch popular movies
+  func fetchMovies() {
+    loading = true
+    guard let url = URL(string: Constant.upcomingUrl) else {
+          print("Invalid URL")
+          return
+      }
+
+      NetworkManager.shared.performRequest(url: url) { (result: Result<MovieResponse, Error>) in
+          switch result {
+          case .success(let movieResponse):
+              DispatchQueue.main.async {
+                  self.movies = movieResponse.results
+                  self.loading = false
+              }
+          case .failure(let error):
+              print("Error fetching movies: \(error)")
+            self.movies = []
+            self.loading = false
+          }
+      }
+  }
+
+}
